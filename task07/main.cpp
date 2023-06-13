021c8af66c5ef88f8cd0e2ac60cf0c1ea8545b7d
RTani99 <tanybrassrock@gmail.com>
2023-06-13 13:57:38 +0900

    task07 finished

--- task07/main.cpp
+++ task07/main.cpp
@@ -26,6 +26,8 @@ void solve_laplace_gauss_sidel_on_grid(
       if (vtx2isfix[idx_center]) { continue; }
       // write some code below to implement Gauss-Sidel method
       // Do not write more than 5 lines of code
+      float sum_edge = vtx2val[idx_center + 1] + vtx2val[idx_center - 1] + vtx2val[idx_center - grid_size] + vtx2val[idx_center + grid_size];
+      vtx2val[idx_center] = sum_edge / 4;
     }
   }
 }
